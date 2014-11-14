ef985f3c0f2faceafe42d46cf8bdcfad3c4eea09
trespassersW <okapi1788@gmail.com>
2014-11-14 19:58:39 +0300

    GitHub Sortable Filelist
    .8 sorting by file extention

--- src/GitHub_Sortable_Filelist.user.js
+++ src/GitHub_Sortable_Filelist.user.js
@@ -3,11 +3,12 @@
 // @namespace   trespassersW
 // @description appends sorting function to github directories
 // @include https://github.com/*
-// @version 14.11.13.7
+// @version 14.11.14.8
+//  .8 sorting by file extention
 //  .7 date/time display mode switching
 //  .4 now works on all github pages
 // @created 2014-11-10
-// @updated 2014-11-13
+// @updated 2014-11-14
 // @author  trespassersW
 // @licence MIT
 // @run-at document-end
@@ -27,10 +28,10 @@ var ii=0,tt;
 var d0=[0,0,1];
 var C=[{c:1, d: 0, s: 0},{c:2, d: 0, s: 0},{c:3, d: 1, s: 0}];
 var ASC;
-var oa=[],ca=[],clock,dtStyle;
+var oa=[],ca=[],clock,ext,dtStyle;
 var D=document, TB;
 var catcher,locStor;
-var prefs={dtStyle:0};
+var prefs={dtStyle:0, ext: 0};
 
 function stickStyle(css){
  var s=document.createElement("style"); s.type="text/css";
@@ -54,54 +55,79 @@ function outerNode(target, node) {
   }catch(e){};
  return null;
 }
+function savePrefs(){
+ if(locStor) locStor.setItem('GHSFL',JSON.stringify(prefs));
+}
 
 function css(){
+
 stickStyle('\
-.fsort-butt, .tables.file  {position: relative; }\
+.fsort-butt,\
+.tables.xxfile td.content, .tables.file td.message, .tables.file td.age\
+ {position: relative; }\
 .fsort-butt:before{\
- position: absolute; left:1.5em; top: -1em; \
+ position: absolute; display: inline-block;\
  cursor: pointer;\
- content: "";\
  z-index:99999;\
- width: 0;  height: 0;\
+ content: "";\
+ width: 16px;  height: 16px;\
+}\
+.fsort-butt.fsort-asc:before,.fsort-butt.fsort-desc:before{\
  opacity:.2;\
- }\
+ left:1.5em; top: -1em;\
+ width: 0;  height: 0;\
+}\
+.fsort-asc:before,.fsort-desc:before{\
+ border-style: solid;\
+ border-color: #654;\
+}\
 .fsort-asc:before,\
 .fsort-desc.fsort-sel:hover:before\
 {\
  border-left: 6px solid transparent;\
  border-right: 6px solid transparent;\
- border-bottom: 14px solid #444;\
- border-top: 0;\
+ border-bottom-width: 14px;\
+ border-top-width: 0;\
 }\
 .fsort-desc:before,\
 .fsort-asc.fsort-sel:hover:before{\
  border-left: 6px solid transparent;\
  border-right: 6px solid transparent;\
- border-bottom: 0;\
- border-top: 14px solid #444;\
+ border-bottom-width: 0;\
+ border-top-width: 14px;\
  }\
-.fsort-butt.fsort-desc.fsort-sel:hover:before,\
-.fsort-butt.fsort-asc.fsort-sel:before{\
- border-bottom: 14px solid #4183C4;\
- border-top: 0;\
- }\
-.fsort-butt.fsort-desc.fsort-sel:before,\
-.fsort-butt.fsort-asc.fsort-sel:hover:before{\
- border-bottom: 0;\
- border-top: 14px solid #4183C4;\
- }\
+\
+.fsort-sel:before,\
+.fsort-sel:before{\
+ border-bottom-color: #4183C4 !important;\
+ border-top-color: #4183C4 !important;\
+}\
 \
 .fsort-butt.fsort-sel:before{ opacity: .6 }\
 .fsort-butt:hover:before{ opacity: 1 !important;}\
 \
-.fsort-butt.fsort-clock:before{\
- position:absolute; left:3em; top:-1.2em;\
- opacity:1!important;\
+#fsort-clock:before{\
+ left:3em; top:-1.2em;\
+ border-radius: 16px;\
+ opacity:1;\
  content: url(data:image/png;base64,\
 iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAlZJREFUeNqkU0tLG1EUvjORFJPMw6mhTUgxy75EUxiJj6CGLKQV0o2IQd3pxp2bbPs/3MVCIHSRgC3uXBhCSErjtLRkK7TcNCYhk4eGPHvO6AwjXXrgu/fOOff77plzz2VGoxF5iI3hEA6HCcuyxGKxEIZhOMBLcHsAwt0+FfAbDvsFaA4GAzIcDkkqlboVMNlzB8+vzgUCq3N+v+xyOl3opFdXNJfN5nPn52dNVT0DV/FeBjpZcjrfR/b3Dzwul0e02ciPiwsyPTtLptxur7C25n0xMzMfPzoSypQmdREWB0iLt3NcKLK3dzAhSR5+fFxT/JbPG+row9g2HABZhpBjCMA/vZYDgRVOFD02q5WwDKOROp2OIYA+jDlgz8Ly8gpyDIF+v++d9vlkXDdubshlpaKhWCwaawTGsHg+WZaRY9QAPiYEUXyinzY1OanNzWbTWGv7gPynViO4FzlmARavcWgivw2FSDAYJDubm4TjOA0Oh4O8WVwk80tLGscsoLZUtWwThGf6afFEgmxvbZGP8biRAbac2u2S60ajjByjBr1e7/JrJqOMQSNhmmiCJGlknHVAAckjuI1cOq0gx3wLyudkMt1ttejgTkAXMRsWkO106KdEIo0cQwAC9b+l0umHaPRYrVaphWX/63n0XasqjR4eHlNKT5FzrxPBoXxXFEtkY6Ozs7u78G59/dVTt/sxdkSJ0uqXk5OfsVgsU6lUUjzPKzqPwdfo8/lIF4pTr9dJu92WID0/tjZAv8MKti48tqzdbq+JUAsrNFWhULgVeIj9E2AAamUckFr2UCoAAAAASUVORK5CYII=\
-);\
-}\
+);}\
+#fsort-clock:before{ background-color: #CCC  }\
+#fsort-clock.fsort-on:before{ background-color: #4183C4  }\
+\
+#fsort-ext:before{\
+ left:4em; top:-14px;\
+ border-radius: 6px;\
+ width:28px;height:16px;\
+ opacity:.6;\
+ content: url(data:image/png;base64,\
+iVBORw0KGgoAAAANSUhEUgAAABoAAAANCAYAAAC3mX7tAAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAAsTAAALEwEAmpwYAAABWUlEQVQ4y+2SP0sDQRDFT7BQTGtnITYSK8Emha1NCrXwC4gWYiUKiiB4jYKdWAmC4AewtxEExSqVjZ1/OEgwrEQwkLvbmd/aTCBI0IBYCD7YYmaYfW/mTRT9VWRZNq6qm79OBBwBrueGPM8ngWvgHbgXkQX7aC+EEFR1x+IDi7dUdQOQYACuviSp1+tDQA2oquoycAlInucT1Wp1EHgA3r3300AG3FUqlX4RKQOvQFNVV0Rk9ksiEZkzRbtJkgx476faqq1etnoK4L0vdazuqefVqepq6ALgKIqiKI7jPuDRcpVPHn1P5JwrxHHc1zHRofe+1H5pmo6akCWrv4QQgojMdxA9AI3vzPfAsXOuACRAXVW3VXUNOM2yrNhsNofNh1qapmPAG5A45wpGdGMiToD9bvdftKZ9I54ALoCGXd5tq9UaAc7Mr0Wbbr09vfk3Azybf+fRP36KDxJ2sN2uMATcAAAAAElFTkSuQmCC\
+);}\
+#fsort-ext:before{ background-color: #CCC}\
+.fsort-on:before{ background-color: #4183C4 !important;}\
+.fsort-on:hover:before{ opacity:1}\
+\
 table.files td.age .css-truncate.css-truncate-target{\
  width: 99% !important; \
  max-width: none !important;\
@@ -111,10 +137,10 @@ table.files td.age span.css-truncate time{\
 }\
 \
 /* patches */\
-table.files td.age {text-align: left !important; padding-: 0 4px !important;\
+table.files td.age {text-align: left !important; padding: 0 4px !important;\
 }\
 table.files td.message {overflow: visible !important;}\
-');//#80A6CD
+');
 
 dtStyle=stickStyle('\
 table.files td.age span.css-truncate time{\
@@ -154,24 +180,30 @@ function isDir(x){
  return (TB.rows[x].cells[0].querySelector("span.octicon-file-directory")) != null;
 }
 
-var sDir,sCells;
+var sDir,sCells,sExts;
  var fa=[
   function(a){
-  sCells.push(TB.rows[a].cells[1].querySelector('span.css-truncate-target a').textContent);
+   return TB.rows[a].cells[1].querySelector('span.css-truncate-target a').textContent;
   },
   function(a){
-   sCells.push(TB.rows[a].cells[2].querySelector('span.css-truncate').textContent);
+   return TB.rows[a].cells[2].querySelector('span.css-truncate').textContent;
   },
   function(a){
-   sCells.push(TB.rows[a].cells[3].querySelector('span.css-truncate>time').getAttribute('datetime'));
+   return TB.rows[a].cells[3].querySelector('span.css-truncate>time').getAttribute('datetime');
   }
  ]
 function sort_p(n){// prepare data for sorting
  sDir=[],sCells=[];
- for(var tl=TB.rows.length, a=0; a<tl; a++){
-   sDir.push(isDir(a));
-   fa[n](a);
- }
+ for(var tl=TB.rows.length, a=0; a<tl; a++) sDir.push(isDir(a));
+ console.log(' sp:'+n+' ext:'+prefs.ext);
+ if( n === 0 && prefs.ext ){
+  for( a=0; a<tl; a++){ // f.x -> x.f
+   var m, x=fa[n](a);
+   m= x.match(/(.*)(\..*)$/);
+   if(m && m.length==3) x=m[2]+m[1];
+   sCells.push(x);
+  }
+ }else for( a=0; a<tl; a++) sCells.push(fa[n](a));
 }
 
 function sort_fn(a,b){ 
@@ -189,17 +221,34 @@ function oClr(){
   o[i].parentNode.removeChild(o[i]);
 }
 //
+function extclassName(){
+  ext.className='fsort-butt'+ (prefs.ext? ' fsort-on': '' );
+}
+function clockclassName(){
+  clock.className='fsort-butt'+ (prefs.dtStyle? '': ' fsort-on');
+}
+//
 function doSort(t){
- if(t.className.indexOf('fsort-clock')>-1){
-   dtStyle.disabled = (1===(prefs.dtStyle ^= 1));
-   if(locStor) locStor.setItem('GHSFL',JSON.stringify(prefs))
-   return;
- }
  TB=outerNode(t,'TBODY');
- var tb=[],ix=[], i, tl;
  if(!TB){  _l( "*GHSFL* TBODY not found"); return; }
  var n=CNn[t.parentNode.className];
  if(typeof n=="undefined"){  _l( "*GHSFL* undefined col"); return; }
+ 
+ if(t.id=='fsort-clock'){
+   dtStyle.disabled = (prefs.dtStyle ^= 1);
+   savePrefs();
+   clockclassName();
+   return;
+ }
+ var xt = (t.id=='fsort-ext');
+ if( xt ){
+  if(C[n].s) prefs.ext ^= 1; 
+  else prefs.ext= 1;
+  savePrefs();
+  extclassName();
+  C[n].d^=C[n].s; // don't toggle dir on ext.click
+ }
+ var tb=[],ix=[], i, tl;
  _l('n:'+n);
  tl=TB.rows.length;
  ASC=C[n].d^=C[n].s;
@@ -243,11 +292,14 @@ function gitDir1(x){
   o=o.cloneNode(true); 
   oa.push(o);
   clock=D.createElement('span');
-  clock.className='fsort-butt fsort-clock';
+  clock.id='fsort-clock'; clockclassName();
+  ext=D.createElement('span');
+  ext.id='fsort-ext'; extclassName();
   setDateTime(); 
   setC(-1);
  }
   insBefore(oa[0],ca[0]);
+  insBefore(ext,ca[0]);
   insBefore(oa[1],ca[1]);
   insBefore(oa[2],ca[2]);
   insBefore(clock,ca[2]);
@@ -289,7 +341,7 @@ if(!MO) MO= window.WebKitMutationObserver;
 if(!MO) return;
 var  observer = new MO(function(mutations) {
   mutations.forEach(function(m) {
-    if( m.type= "attributes" &&  
+    if( m.type== "attributes" &&  
         m.target.nodeName == 'DIV' &&  
         m.target.className == "file-wrap" )
       gitDir();
@@ -305,7 +357,6 @@ observer.observe(D.body, { attributes: true, subtree: true } );
 */
 
 })()};
-
 /*
  to do: persistent settings; sorting by file extensions; toggling date/time display mode 
  ... do we really need it?
